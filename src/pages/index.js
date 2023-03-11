769aca6fa2ceb2890087584df85994e559da6ea1
danusaputra <danusaputraoffice@gmail.com>
2023-03-11 15:53:52 +0700

    Update card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Card, Space, Statistic, Typography } from "antd";
 import { Paper } from "@mui/material";
 import BaseLayout from "../components/BaseLayout";
@@ -12,7 +13,10 @@ const Dashboard = () => {
         <Space direction="horizontal">
           <Card
             className="card"
-            style={{ boxShadow: "5px 8px 24px 5px #A9A9A9" }}
+            style={{
+              boxShadow: "2px 3px 15px 3px #A9A9A9",
+              position: "relative",
+            }}
           >
             <BsThreeDotsVertical
               style={{
@@ -23,22 +27,87 @@ const Dashboard = () => {
             />
             <Paper
               style={{
-                backgroundColor: "#fff579",
-                width: "39px",
-                height: "39px",
+                backgroundColor: "#fffff",
+                width: "50px",
+                height: "40px",
+                borderRadius: "10%",
                 position: "absolute",
-                borderRadius: "80%"
+                top: 24,
+                left: 20,
               }}
             />
             <BsFillCreditCardFill
               style={{
                 color: "orange",
-                fontSize: "2.2rem",
+                fontSize: "2.1rem",
                 position: "absolute",
+                top: "1.6rem",
+                left: "1.7rem",
               }}
             />
             <p className="kas">KAS</p>
-            <Statistic title="Total Saldo" value={"Rp.6.000.000,00"} />
+            <Statistic title="Total Saldo" value={"Rp. 6.000.000,00"} />
+          </Card>
+        </Space>
+
+        <Space direction="horizontal">
+          <Card
+            className="card"
+            style={{
+              boxShadow: "2px 3px 15px 3px #A9A9A9",
+              position: "relative",
+            }}
+          >
+            <BsThreeDotsVertical
+              style={{
+                marginLeft: "16rem",
+                fontSize: "1.5rem",
+                position: "absolute",
+              }}
+            />
+            <Image
+              width={50}
+              height={40}
+              className="cardbri_logo"
+              src="/BRI.jpg"
+              alt="logo"
+              position="absolute"
+            />
+            <Statistic
+              classname="title"
+              title="Total Saldo"
+              value={"Rp. 5.920.000,00"}
+            />
+          </Card>
+        </Space>
+        <Space direction="horizontal">
+          <Card
+            className="card"
+            style={{
+              boxShadow: "2px 3px 15px 3px #A9A9A9",
+              position: "relative",
+            }}
+          >
+            <BsThreeDotsVertical
+              style={{
+                marginLeft: "16rem",
+                fontSize: "1.5rem",
+                position: "absolute",
+              }}
+            />
+            <Image
+              width={50}
+              height={40}
+              className="cardmandiri_logo"
+              src="/mandiri.png"
+              alt="logo"
+              position="absolute"
+            />
+            <Statistic
+              classname="title"
+              title="Total Saldo"
+              value={"Rp. 6.920.000,00"}
+            />
           </Card>
         </Space>
       </div>
