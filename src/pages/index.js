7b80e3a4809102460af9d01432c155737f162b95
danusaputra <danusaputraoffice@gmail.com>
2023-03-11 12:55:48 +0700

    Update Card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { Card, Space, Statistic, Typography } from "antd";
+import { Paper } from "@mui/material";
 import BaseLayout from "../components/BaseLayout";
 import React from "react";
 import { BsFillCreditCardFill, BsThreeDotsVertical } from "react-icons/bs";
@@ -9,10 +10,35 @@ const Dashboard = () => {
       <div className="dashboard">
         <h1>Dashboard</h1>
         <Space direction="horizontal">
-          <Card className="card">
-            <BsFillCreditCardFill style={{ color: "orange" }} />
-            <BsThreeDotsVertical style={{ marginLeft: "10rem"}}/>
-            <Statistic title="Kas" value={"Rp.6.000.000,00"} />
+          <Card
+            className="card"
+            style={{ boxShadow: "5px 8px 24px 5px #A9A9A9" }}
+          >
+            <BsThreeDotsVertical
+              style={{
+                marginLeft: "16rem",
+                fontSize: "1.5rem",
+                position: "absolute",
+              }}
+            />
+            <Paper
+              style={{
+                backgroundColor: "#fff579",
+                width: "39px",
+                height: "39px",
+                position: "absolute",
+                borderRadius: "80%"
+              }}
+            />
+            <BsFillCreditCardFill
+              style={{
+                color: "orange",
+                fontSize: "2.2rem",
+                position: "absolute",
+              }}
+            />
+            <p className="kas">KAS</p>
+            <Statistic title="Total Saldo" value={"Rp.6.000.000,00"} />
           </Card>
         </Space>
       </div>
